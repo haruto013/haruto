ce1a1c7a40af364ed96c4cdddd9e2b8743194edd
haruto013 <toruhasoccer@gmail.com>
2022-04-19 20:55:08 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,18 @@
 
 
 
-Route::get('/', 'PostController@index');
+Route::get('/posts', 'PostController@index');
+Route::post('/posts', 'PostController@store');
+Route::get('/posts/create', 'PostController@create');
+
+
+
+
+
 
 Route::get('/posts/{post}', 'PostController@show');
 
-Route::get('/posts/create', 'PostController@create');
+
 
 ?>
 
